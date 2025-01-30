de281b1db83085602ba8f074458d3335009062db
StevenDelval <asteven59220@free.fr>
2025-01-30 13:12:56 +0100

    Add module for azure data factory

---
+++ Terraform/modules/pipeline_data_factory/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_data_factory" "data_factory" {
+  name                     = var.data_factory_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+}
