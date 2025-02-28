b0a47a11ad6b1b3282289a518f86957b9739d803
StevenDelval <asteven59220@free.fr>
2025-02-28 13:24:45 +0100

    Add function to get Datalake client

--- azure_functions/collecte_api/function_app.py
+++ azure_functions/collecte_api/function_app.py
@@ -1,11 +1,42 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+from dotenv import load_dotenv
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import json
+import os
+from azure.storage.filedatalake import DataLakeServiceClient
+from io import BytesIO
 
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Configuration pour Azure Data Lake
+STORAGE_ACCOUNT_NAME = os.getenv("STORAGE_ACCOUNT_NAME")
+STORAGE_ACCOUNT_KEY = os.getenv("STORAGE_ACCOUNT_KEY")
+FILE_SYSTEM_NAME = os.getenv("FILE_SYSTEM_NAME")
+DIRECTORY_NAME = os.getenv("DIRECTORY_NAME")
+
+def get_service_client(account_name: str, account_key: str) -> DataLakeServiceClient:
+    """Crée une instance de DataLakeServiceClient.
+    
+    Args:
+        account_name (str): Nom du compte de stockage Azure.
+        account_key (str): Clé d'accès au compte de stockage.
+    
+    Returns:
+        DataLakeServiceClient: Client pour interagir avec le service Azure Data Lake.
+    """
+    return DataLakeServiceClient(
+        account_url=f"https://{account_name}.dfs.core.windows.net",
+        credential=account_key
+    )
+
+# Création de l'application Azure Function
 app = func.FunctionApp()
 
-@app.route(route="collecte_api_hubeau_data", auth_level=func.AuthLevel.Function)
+@app.route(route="collecte_api_hubeau_data", auth_level=func.AuthLevel.Function, methods=["POST"])
 def collecte_api_hubeau_data(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
