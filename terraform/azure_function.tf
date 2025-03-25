3224b6ab913b0000c6d5a77cecf5586a869a4a21
StevenDelval <asteven59220@free.fr>
2025-03-25 09:28:40 +0100

    add api function

--- terraform/azure_function.tf
+++ terraform/azure_function.tf
@@ -41,3 +41,31 @@ module "azure_functions_weather_data"{
     log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
     depends_on = [ azurerm_storage_container.function_code_container ]
 }
+
+module "azure_functions_api" {
+    source = "./modules/azure_functions"
+
+    service_plan_func_name = "sp-api-hubeau-func-sd"
+    function_name = "get-hubeau-api-data-projet-sd"
+    
+    resource_group_location = azurerm_resource_group.resource_group.location
+    resource_group_name = azurerm_resource_group.resource_group.name
+    
+    function_storage = azurerm_storage_account.function_code_blob.name
+    function_storage_primary_access_key = azurerm_storage_account.function_code_blob.primary_access_key
+    azurerm_storage_account_connection_string = azurerm_storage_account.function_code_blob.primary_connection_string
+    function_source_dir = "../azure_functions/collecte_api"
+
+    application_insights_connection_string = azurerm_application_insights.app_insights.connection_string
+    application_insights_key = azurerm_application_insights.app_insights.instrumentation_key
+    
+    app_settings = {
+        "STORAGE_ACCOUNT_NAME" = azurerm_storage_account.data_lake.name
+        "STORAGE_ACCOUNT_KEY" = azurerm_storage_account.data_lake.primary_access_key
+        "DIRECTORY_NAME" = "donnees-piezometre"
+        "FILE_SYSTEM_NAME" = "quotidien"
+        }
+
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+    depends_on = [ azurerm_storage_container.function_code_container ]
+}
