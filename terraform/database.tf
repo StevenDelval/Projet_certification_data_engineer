f4329cd1c31e84c4b3386a5fe3af2e9571e7c348
StevenDelval <asteven59220@free.fr>
2025-03-26 14:01:31 +0100

    add database for data

--- terraform/database.tf
+++ terraform/database.tf
@@ -7,7 +7,7 @@ resource "azurerm_mssql_server" "sql_server" {
   administrator_login_password = var.admin_password 
 }
 
-resource "azurerm_mssql_database" "db" {
+resource "azurerm_mssql_database" "db_control_table" {
   name           = "databaseControlTables"
   server_id      = azurerm_mssql_server.sql_server.id
   sku_name       = "Free"
@@ -29,7 +29,7 @@ resource "null_resource" "initialize_db" {
       sqlcmd -S my-sqlserver-sd.database.windows.net -U ${var.admin_login} -P "${var.admin_password}" -d databaseControlTables -i ../base_de_donnees/control_tables.sql
     EOT
   }
-  depends_on = [azurerm_mssql_database.db]
+  depends_on = [azurerm_mssql_database.db_control_table]
 }
 
 resource "null_resource" "procedure" {
@@ -38,5 +38,13 @@ resource "null_resource" "procedure" {
       sqlcmd -S my-sqlserver-sd.database.windows.net -U ${var.admin_login} -P "${var.admin_password}" -d databaseControlTables -i ../base_de_donnees/ct_storage_procedure.sql
     EOT
   }
-  depends_on = [azurerm_mssql_database.db]
+  depends_on = [azurerm_mssql_database.db_control_table]
+}
+
+resource "azurerm_mssql_database" "db_data" {
+  name           = "databaseDataToExpose"
+  server_id      = azurerm_mssql_server.sql_server.id
+  sku_name       = "Basic"
+  zone_redundant = false
+  storage_account_type = "Local"
 }
