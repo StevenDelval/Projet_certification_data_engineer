632613622782f6ecafe2cc149f5a92e9ccca2bc7
StevenDelval <asteven59220@free.fr>
2025-03-26 13:01:30 +0100

    Change name file

--- base_de_donnees/control_tables.sql
+++ base_de_donnees/control_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE CsvControlTable (
+CREATE TABLE  CsvControlTable (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     nom_du_fichier NVARCHAR(100),
     url_du_fichier NVARCHAR(200),
@@ -7,9 +7,9 @@ CREATE TABLE CsvControlTable (
 
 INSERT INTO CsvControlTable (nom_du_fichier, url_du_fichier, annee_de_fin)
     VALUES
-    ('QUOT_SIM2_2000-2009', 'https://www.data.gouv.fr/fr/datasets/r/10d2ce77-5c3b-44f8-bb46-4df27ed48595', 2009),
-    ('QUOT_SIM2_2010-2019', 'https://www.data.gouv.fr/fr/datasets/r/da6cd598-498b-4e39-96ea-fae89a4a8a46', 2019),
-    ('QUOT_SIM2_2020-2029', 'https://www.data.gouv.fr/fr/datasets/r/92065ec0-ea6f-4f5e-8827-4344179c0a7f', 2029);
+    ('QUOT_SIM2_2000-2009.parquet', 'https://www.data.gouv.fr/fr/datasets/r/10d2ce77-5c3b-44f8-bb46-4df27ed48595', 2009),
+    ('QUOT_SIM2_2010-2019.parquet', 'https://www.data.gouv.fr/fr/datasets/r/da6cd598-498b-4e39-96ea-fae89a4a8a46', 2019),
+    ('QUOT_SIM2_2020-2029.parquet', 'https://www.data.gouv.fr/fr/datasets/r/92065ec0-ea6f-4f5e-8827-4344179c0a7f', 2029);
 
 CREATE TABLE ApiControlTable (
     ID INT IDENTITY(1,1) PRIMARY KEY,
