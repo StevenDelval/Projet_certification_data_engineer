f9801efc4c9a94212aff3fa58553c6818e34635a
StevenDelval <asteven59220@free.fr>
2025-03-26 11:53:31 +0100

    Change json

--- terraform/datafactory.tf
+++ terraform/datafactory.tf
@@ -30,102 +30,114 @@ resource "azurerm_data_factory_dataset_sql_server_table" "table_control_for_csv"
 resource "azurerm_data_factory_pipeline" "pipeline_get_csv" {
   name                    = "pipeline_get_csv"
   data_factory_id = azurerm_data_factory.data_factory.id
-  activities_json         = jsonencode([
-    {
-      name = "get_info_csv"
-      type = "Lookup"
-      dependsOn = []
-      policy = {
-        timeout                  = "0.12:00:00"
-        retry                    = 0
-        retryIntervalInSeconds   = 30
-        secureOutput             = false
-        secureInput              = false
-      }
-      typeProperties = {
-        source = {
-          type            = "AzureSqlSource"
-          sqlReaderQuery  = "SELECT nom_du_fichier, url_du_fichier FROM CsvControlTable"
-          queryTimeout    = "02:00:00"
-          partitionOption = "None"
-        }
-        dataset = {
-          referenceName = azurerm_data_factory_dataset_sql_server_table.table_control_for_csv.name
-          type          = "DatasetReference"
-        }
-        firstRowOnly = false
-      }
-    },
-    {
-      name = "ForEach1"
-      type = "ForEach"
-      dependsOn = [
-        {
-          activity = "get_info_csv"
-          dependencyConditions = [ "Succeeded" ]
-        }
-      ]
-      typeProperties = {
-        items = {
-          value = "@activity('get_info_csv').output.value"
-          type  = "Expression"
-        }
-        isSequential = false
-        batchCount   = 1
-        activities = [
-          {
-            name = "Azure Function Get csv"
-            type = "AzureFunctionActivity"
-            dependsOn = []
-            policy = {
-              timeout                  = "0.12:00:00"
-              retry                    = 0
-              retryIntervalInSeconds   = 30
-              secureOutput             = false
-              secureInput              = false
-            }
-            typeProperties = {
-              functionName = "collecte_csv_weather_data"
-              body = {
-                value = "{\n    \"file_name\":\"@{item().nom_du_fichier}\",\n    \"url_csv_file\":\"@{item().url_du_fichier}\"\n    }"
-                type  = "Expression"
-              }
-              method = "POST"
-            }
-            linkedServiceName = {
-              referenceName = azurerm_data_factory_linked_service_azure_function.csv_function.name
-              type          = "LinkedServiceReference"
-            }
+  activities_json         = <<JSON
+  [
+      {
+          "name": "get_info_csv",
+          "type": "Lookup",
+          "dependsOn": [],
+          "policy": {
+              "timeout": "0.12:00:00",
+              "retry": 0,
+              "retryIntervalInSeconds": 30,
+              "secureOutput": false,
+              "secureInput": false
+          },
+          "userProperties": [],
+          "typeProperties": {
+              "source": {
+                  "type": "AzureSqlSource",
+                  "sqlReaderQuery": "SELECT nom_du_fichier, url_du_fichier FROM CsvControlTable",
+                  "queryTimeout": "02:00:00",
+                  "partitionOption": "None"
+              },
+              "dataset": {
+                  "referenceName": "${azurerm_data_factory_dataset_sql_server_table.table_control_for_csv.name}",
+                  "type": "DatasetReference"
+              },
+              "firstRowOnly": false
           }
-        ]
+      },
+      {
+          "name": "ForEach1",
+          "type": "ForEach",
+          "dependsOn": [
+              {
+                  "activity": "get_info_csv",
+                  "dependencyConditions": [
+                      "Succeeded"
+                  ]
+              }
+          ],
+          "userProperties": [],
+          "typeProperties": {
+              "items": {
+                  "value": "@activity('get_info_csv').output.value",
+                  "type": "Expression"
+              },
+              "isSequential": false,
+              "batchCount": 1,
+              "activities": [
+                  {
+                      "name": "Azure Function Get csv",
+                      "type": "AzureFunctionActivity",
+                      "dependsOn": [],
+                      "policy": {
+                          "timeout": "0.12:00:00",
+                          "retry": 0,
+                          "retryIntervalInSeconds": 30,
+                          "secureOutput": false,
+                          "secureInput": false
+                      },
+                      "userProperties": [],
+                      "typeProperties": {
+                          "functionName": "collecte_csv_weather_data",
+                          "body": {
+                              "value": "{\n    \"file_name\":\"@{item().nom_du_fichier}\",\n    \"url_csv_file\":\"@{item().url_du_fichier}\"\n    }",
+                              "type": "Expression"
+                          },
+                          "method": "POST"
+                      },
+                      "linkedServiceName": {
+                          "referenceName": "${azurerm_data_factory_linked_service_azure_function.csv_function.name}",
+                          "type": "LinkedServiceReference"
+                      }
+                  }
+              ]
+          }
+      },
+      {
+          "name": "DeleteUselessRow",
+          "type": "SqlServerStoredProcedure",
+          "dependsOn": [
+              {
+                  "activity": "ForEach1",
+                  "dependencyConditions": [
+                      "Succeeded"
+                  ]
+              }
+          ],
+          "policy": {
+              "timeout": "0.12:00:00",
+              "retry": 0,
+              "retryIntervalInSeconds": 30,
+              "secureOutput": false,
+              "secureInput": false
+          },
+          "userProperties": [],
+          "typeProperties": {
+              "storedProcedureName": "[dbo].[DeleteUselessRow]"
+          },
+          "linkedServiceName": {
+              "referenceName": "${azurerm_data_factory_linked_service_sql_server.csv_database.name}",
+              "type": "LinkedServiceReference"
+          }
       }
-    },
-    {
-      name = "DeleteUselessRow"
-      type = "SqlServerStoredProcedure"
-      dependsOn = [
-        {
-          activity = "ForEach1"
-          dependencyConditions = [ "Succeeded" ]
-        }
-      ]
-      policy = {
-        timeout                  = "0.12:00:00"
-        retry                    = 0
-        retryIntervalInSeconds   = 30
-        secureOutput             = false
-        secureInput              = false
-      }
-      typeProperties = {
-        storedProcedureName = "[dbo].[DeleteUselessRow]"
-      }
-      linkedServiceName = {
-        referenceName = azurerm_data_factory_linked_service_sql_server.csv_database.name
-        type          = "LinkedServiceReference"
-      }
-    }
-  ])
+  ]
+  JSON
 }
+
+
 
     
 resource "azurerm_data_factory_trigger_schedule" "trigger_get_csv" {
@@ -137,7 +149,7 @@ resource "azurerm_data_factory_trigger_schedule" "trigger_get_csv" {
   }
   frequency = "Month"
   interval  = 1
-  start_time = "2025-04-08T10:00:00Z" # Use UTC time to avoid timezone issues
+  start_time = "2025-04-08T10:00:00Z" 
   time_zone  = "Romance Standard Time"
   schedule {
       minutes   = [0]
