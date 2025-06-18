38f278a6aba96bb580208b0ebae2beee04a80266
StevenDelval <asteven59220@free.fr>
2025-06-18 14:03:42 +0200

    Add table from automap

--- api/models.py
+++ api/models.py
@@ -1,16 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date, Boolean, BigInteger
+from sqlalchemy.ext.automap import automap_base
 from database import Base, engine
 
-# Define column types based on the database engine
-if engine.dialect.name == 'sqlite':
-    date_type = String
-    bool_type = Integer
-    big_int_type = Integer
-    
-else:
-    date_type = Date
-    bool_type = Boolean
-    big_int_type = BigInteger
 
 class User(Base):
     """
@@ -35,3 +26,13 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+AutoBase = automap_base()
+AutoBase.prepare(engine, reflect=True)
+
+TableMeteoQuotidien = AutoBase.classes.TableMeteoQuotidien
+TablePiezoInfo = AutoBase.classes.TablePiezoInfo
+Nature_mesure = AutoBase.classes.Nature_mesure
+Continuite = AutoBase.classes.Continuite
+Producteur = AutoBase.classes.Producteur
+TablePiezoQuotidien = AutoBase.classes.TablePiezoQuotidien
