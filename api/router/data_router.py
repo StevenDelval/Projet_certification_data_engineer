f0444936105fee534fa1e12a17db111a5071ecc7
StevenDelval <asteven59220@free.fr>
2025-06-19 15:37:56 +0200

    feat: add endpoint for weather data and piezo data

--- api/router/data_router.py
+++ api/router/data_router.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
+from typing import List, Annotated
+from fastapi import FastAPI, Depends, HTTPException, status, APIRouter, Query
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
 from database import get_db
@@ -10,23 +11,47 @@ import crud, auth, schemas
 router = APIRouter()
 
 
-@router.post("/hello")
-def get_hello():
+@router.post("/meteo/", response_model=schemas.MeteoDataOut)
+def read_meteo_point(request: schemas.MeteoRequest, db: Session = Depends(get_db)):
     """
-    Simple endpoint that returns a greeting message.
+    Retrieve daily weather data for a given date and Lambert coordinates.
 
-    This endpoint is a basic test route that returns a static string message.
+    Args:
+        request (MeteoRequest): JSON payload with `date`, `lambx`, and `lamby`.
 
     Returns:
-        str: A greeting message "Hello !".
+        MeteoDataOut: Weather data record.
 
-    Notes:
-        This endpoint does not perform any database operations or require any request body.
-        It is primarily used for testing and ensuring that the API is accessible.
+    Raises:
+        404: If no matching data is found.
     """
-    return "Hello !"
+    meteo_data = crud.get_meteo_by_date_coords(db, request.input_date, request.lambx, request.lamby)
+
+    if not meteo_data:
+        raise HTTPException(status_code=404, detail="No weather data found for the given point and date.")
+
+    return meteo_data
+
+@router.post("/piezo_value/", response_model=schemas.PiezoPaginatedResponse)
+def read_piezo_value(
+        request: schemas.PiezoRequest,
+        db: Session = Depends(get_db),
+        limit: int = Query(50, ge=1, le=1000),
+        offset: int = Query(0, ge=0)
+    ):
+
+    total, results = crud.get_paginated_piezo_data_by_code_bss(
+        db, code_bss=request.code_bss, offset=offset, limit=limit
+    )
+
+    if not results:
+        raise HTTPException(status_code=404, detail="No piezo data found for the given code_bss.")
+
+    return schemas.PiezoPaginatedResponse(
+        total=total,
+        limit=limit,
+        offset=offset,
+        results=results
+    )
 
 
-@router.get("/meteo/{date}/{lambx}/{lamby}")
-def read_meteo_point(date: str, lambx: int, lamby: int, db: Session = Depends(get_db)):
-    return crud.get_meteo_by_date_coords(db, date, lambx, lamby)
