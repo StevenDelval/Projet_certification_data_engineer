ec79b0080f417d0f047e8b22eacc387f7a89639b
StevenDelval <asteven59220@free.fr>
2026-01-20 13:16:52 +0100

    add provider databricks

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = ">= 3.89, < 4.0.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.50.0"
     }
   }
 }
@@ -11,11 +15,7 @@ terraform {
 provider "azurerm" {
   features {}
 }
-provider "archive" {
-}
-provider "null" {
-  
-}
-provider "template" {
-  
+
+provider "databricks" {
+  azure_workspace_resource_id = azurerm_databricks_workspace.databricks_workspace.id
 }
